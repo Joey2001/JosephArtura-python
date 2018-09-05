366139562ae8b1d0ec81fedd22b89374d51d874f
Joseph <josephartura@gmail.com>
2018-09-05 09:52:59 -0700

    Pika Pika

--- HelloTacoma/HelloTacoma.py
+++ HelloTacoma/HelloTacoma.py
@@ -1,5 +1,8 @@
-Pika = " (\\__/)\n (*^.^)\nz(_(\")(\")"
-hell0 = "Hello"
+Pika = " (\\__/)\n (*^.^)\nz(_(\")(\")\n"
+Pika1 = "  (\\__/) "
+Pika2 = "  (*^.^) "
+Pika3 = "z(_(\")(\")"
+hell0 = "Hello Tacoma"
 print(hell0)
 print("hello") # This is a comment
 #my_String = "Hello Tacoma"
@@ -12,5 +15,12 @@ print("hello") # This is a comment
 #for i in my_String:
 #    x = x-1
 #    print(my_String[0:x])
-for i in range(20):
-    print(Pika)
+#for i in range(200):
+#    print(Pika)
+Pikas = 2
+Pikus = 2
+for i in range(Pikus):
+    print(Pikas * Pika1)
+    print(Pikas * Pika2)
+    print(Pikas * Pika3)
+print("There have been", (Pikas*Pikus), "Pikas printed")
