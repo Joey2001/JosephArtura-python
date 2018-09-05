70b532d8714ed2568ce3b4d23be1716125f4ef61
Joseph <josephartura@gmail.com>
2018-09-05 09:25:21 -0700

    Hello Tacoma

---
+++ HelloTacoma/HelloTacoma.py
@@ -0,0 +1,16 @@
+Pika = " (\\__/)\n (*^.^)\nz(_(\")(\")"
+hell0 = "Hello"
+print(hell0)
+print("hello") # This is a comment
+#my_String = "Hello Tacoma"
+
+#x=0
+
+#for i in my_String:
+#    x = x+1
+#    print(my_String[0:x])
+#for i in my_String:
+#    x = x-1
+#    print(my_String[0:x])
+for i in range(20):
+    print(Pika)
