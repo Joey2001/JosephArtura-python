4b4ea507ed35dea4845901b5e18c709f94c97b21
Joseph <josephartura@gmail.com>
2018-09-11 09:18:24 -0700

    Lab 2A

---
+++ Unit2Alab/Unit2Alab.py
@@ -0,0 +1,29 @@
+x_one = 3
+y_one = 4.5577
+print(x_one + y_one)
+
+aNumber = int(input("What is your desired number?\n"))
+print(aNumber)
+
+
+i = 100
+j = 200
+x = 300
+y = 100
+
+if i == j or i>x:
+    print("The variable i is equal to j or i is greater than x")
+
+if i<j and i>=y:
+    print('the variable i is less than j and i is greater than or equal to y')
+
+if i != j and i!=x:
+    print('i is not equal to j and/or x')
+
+if i<j and i<x and i<y:
+    print('i is less than j, x, and y')
+
+
+num1 = float(input("What is your first desired number?\n"))
+num2 = float(input("What is your second desired number?\n"))
+print("The output is:", num1 + num2)
