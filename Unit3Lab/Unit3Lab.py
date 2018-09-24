c8b2db793cbd78521506bd9622e2d9499570acce
Joseph <josephartura@gmail.com>
2018-09-24 08:23:42 -0700

    Something

--- Unit3Lab/Unit3Lab.py
+++ Unit3Lab/Unit3Lab.py
@@ -8,31 +8,38 @@ i = 6
 j = 10
 
 def mySchool():
-    print("I go to Bellarmine Prep and I like AP Computer Science A")
+    return "I go to Bellarmine Prep and I like AP Computer Science A"
 
 def calcGrade(grade):
-    print("You have been in school for",grade,"years")
+    return "You have been in school for",grade,"years"
 
 def City(city, year):
     print("You live in",city,"and you are in grade", year)
 
 def Recieving(z):
-    print(z)
+    return z
 
 def twoNumbers(a,b):
-    print(random.randint(a,b))
+    return random.randint(a,b)
 
 def areaPlane(a,b):
-    print("The box has an area of", (a*b), "units^2")
+    return "The box has an area of", (a*b), "units^2"
 
 def Volume(a,b,c):
-    print("The volume of the box is ",(a*b*c), "units^3")
+    return "The volume of the box is ",(a*b*c), "units^3"
 
-areaPlane(float(input("What is the length? - ")), float(input("What is the width? - ")))
-Volume(float(input("What is the length? - ")),float(input("What is the width? - ")),float(input("What is the height? - ")))
-twoNumbers(float(input("What is your first number? - ")),float(input("What is your second number? - ")))
-Recieving(input("What number?"))
-myFunc(i,j)
-mySchool()
-calcGrade(int(input("What grade are you in? - ")))
+print(areaPlane(float(input("What is the length? - ")), float(input("What is the width? - "))))
+
+print(Volume(float(input("What is the length? - ")),float(input("What is the width? - ")),float(input("What is the height? - "))))
+
+print(twoNumbers(float(input("What is your first number? - ")),float(input("What is your second number? - "))))
+
+print(Recieving(input("What number?")))
+
+print(myFunc(i,j))
+
+print(mySchool())
+
+print(calcGrade(int(input("What grade are you in? - "))))
+
 City(input("What city do you live in? - ", int(input("What grade are you in? - "))))
