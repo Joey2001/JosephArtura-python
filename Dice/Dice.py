b49d01b888c0cd5d3f8b21b16bab00595f95c937
joey2001 <19arturajoseph@bprep.org>
2018-11-02 09:20:28 -0700

    Dice

--- Dice/Dice.py
+++ Dice/Dice.py
@@ -1,11 +1,12 @@
 import random
-DICENUM = 10
+DICENUM = int(input("How many dice do you want to roll? - "))
 def main():
     count = 0
+    limit = int(input("How many times do you want to roll each dice? - "))
     countDice = []
     play = 'y'
     setOfDice = constructDice()
-    while(count < 10000000):
+    while(count < limit):
         diceSet = [0] * DICENUM
         addSets = []
         for i in range(DICENUM):
@@ -20,11 +21,11 @@ def main():
     numberCount(countDice)
         #play = input("Do you want to play again? (\'y\' or \'n\') - ")
     if(count > 1):
-        print("You have played", count, "times.")
+        print("You have rolled", count, "times.")
     elif(count == 1):
-        print("You have played one time.")
+        print("You have rolled one time.")
     else:
-        print("You have not played.")
+        print("You have not rolled.")
 
 def constructDice():
     topBottom = ' ------- '
